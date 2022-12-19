485288e4a30ae255d8a0fd87337eed8b339e7bee
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2022-12-20 00:19:12 +0530

    Add API functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import './style.css';
+import {
+  refresh, form, player, score,
+} from './modules/declares.js';
+import getScores from './modules/getScores.js';
 
-const scores = `<span>Name: 100</span>
-<span>Name: 50</span>
-<span>Name: 100</span>
-<span>Name: 50</span>
-<span>Name: 100</span>
-<span>Name: 50</span>`;
+getScores();
 
-document.querySelector('#scores').innerHTML = scores;
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6HRG9GY8pYokmWJqH497/scores/', {
+    method: 'POST',
+    body: JSON.stringify({
+      user: player.value,
+      score: score.value,
+    }),
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  });
+  window.location.reload();
+});
+
+refresh.addEventListener('click', () => {
+  window.location.reload();
+});
