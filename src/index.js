8450742933dd8b7956d04160c1c3a2bbecba5945
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2022-12-19 19:17:05 +0530

    Fix linters error and styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+const scores = `<span>Name: 100</span>
+<span>Name: 50</span>
+<span>Name: 100</span>
+<span>Name: 50</span>
+<span>Name: 100</span>
+<span>Name: 50</span>`;
+
+document.querySelector('#scores').innerHTML = scores;
